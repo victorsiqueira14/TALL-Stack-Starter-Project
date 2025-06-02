3fa8902177e5e708fbde2b22ec27721993e2f6bf
Victor Siqueira <victorms.adm@gmail.com>
2025-06-02 19:31:36 -0300

    Cast email request input to string in ResetPassword component

--- app/Livewire/Auth/ResetPassword.php
+++ app/Livewire/Auth/ResetPassword.php
@@ -33,7 +33,7 @@ final class ResetPassword extends Component
     {
         $this->token = $token;
 
-        $this->email = request()->string('email');
+        $this->email = (string) request()->string('email');
     }
 
     /**
