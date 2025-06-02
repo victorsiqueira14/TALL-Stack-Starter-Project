1fb7a42085e6f3b3a2f184f9a096e64d5a45baf9
Victor Siqueira <victorms.adm@gmail.com>
2025-06-02 19:20:54 -0300

    Refactor imports and ensure consistent newline at end of files in various test cases

--- tests/Arch/UnguardTest.php
+++ tests/Arch/UnguardTest.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 it('Unguard is enabled by default', function (): void {
     $model = new class extends Model {};
-    
+
     $model->fill(['non_fillable_attribute' => 'value']);
-    
+
     expect($model->non_fillable_attribute)->toBe('value');
 });
