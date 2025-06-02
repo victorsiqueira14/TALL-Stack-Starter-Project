a08fc74d2f9b1cdb76257235f5e51c2da5a22b79
Victor Siqueira <victorms.adm@gmail.com>
2025-06-02 19:20:08 -0300

    Add UnguardTest to verify default behavior of model fill protection

---
+++ tests/Arch/UnguardTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Arch;
+
+use Illuminate\Database\Eloquent\Model;
+
+it('Unguard is enabled by default', function (): void {
+    $model = new class extends Model {};
+    
+    $model->fill(['non_fillable_attribute' => 'value']);
+    
+    expect($model->non_fillable_attribute)->toBe('value');
+});
