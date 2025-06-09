f30e13d70a31ab32543b15eeeb4fc3bf12215c21
Victor Siqueira <victorms.adm@gmail.com>
2025-06-09 19:12:33 -0300

    Adding Telescope

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,4 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
+    ->withSchedule(function ($schedule): void {
+        $schedule->command('telescope:prune')->daily();
     })->create();
